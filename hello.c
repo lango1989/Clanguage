a1a83f610519d6b95a161389aa2a66ea1c2dfcd4
lango1989 <yaowenlang1989@163.com>
2016-09-17 21:55:25 +0800

    add hello.c

--- hello.c
+++ hello.c
@@ -1,29 +1,7 @@
 #include <stdio.h>
-void foo(void);
-int a=10;
 
 int main()
 {
-	a++;
-	printf("a=%d", a);
-	foo();
+	printf("Hello world!\n");
 	return 0;
 }
-
-void foo(void){
-#printf(" hello world!");
-#printf(" hello world!");
-###	hello.c
-#	prinuf(" hello world!");
-#
-#####prinuf("hello world!");
-}
-
-
-01	02	03	04	05
-11	12	13	14	15
-21	22	23	24	25
-31	32	33	34	35
-41	42	43	44	45
-51	52	53	53	55
-61	62	63	64	65
